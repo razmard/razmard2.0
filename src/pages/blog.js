a0234912107ea6079ba316eff31e931c00da28f3
razmard <razmard33@gmail.com>
2022-04-20 10:48:24 +0430

    finished it

---
+++ src/pages/blog.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import BlogCard from "../components/blogcard";
+import SEO from "../components/seo";
+import { Grid } from "@mui/material";
+
+const BlogMain = ({data}) => {
+ const blogposts = data.allContentfulBlogPost.edges
+
+  return (
+      
+      
+      <Layout>
+        <SEO title="Blog" image="/images/Dreamer-#4113.png" />
+        <Grid spacing={1} container gridTemplateColumns="repeat(1, 1fr)" sx={{justifyContent: "center",}}>
+
+        {blogposts.map( blogpost => <BlogCard title={blogpost.node.title} description={blogpost.node.subtitle} image={blogpost.node.thumbnail} slug={blogpost.node.slug}/> )}
+        </Grid>
+        </Layout>
+  )
+}
+
+export default BlogMain
+
+export const pageQuery = graphql`
+query blogpostpagequery {
+    allContentfulBlogPost {
+      edges {
+        node {
+          slug
+          subtitle
+          title
+          thumbnail {
+            gatsbyImageData(placeholder: TRACED_SVG)
+          }
+        }
+      }
+    }
+  }
+  `
